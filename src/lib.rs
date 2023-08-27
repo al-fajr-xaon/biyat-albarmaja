248083b7c75c0321ff4c6c58daa6e8ea792ffe1c
Rayyan K <117920879+rkx7@users.noreply.github.com>
2023-08-27 15:41:52 -0400

    Added Rust, Svelte, Electron, TypeScript, and SCSS

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use neon::prelude::*;
+
+fn get_num_cpus(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(cx.number(10))
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("get", get_num_cpus)?;
+    Ok(())
+}
